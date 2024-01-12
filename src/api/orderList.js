3b078a93843c6c3543e63c22c786db1bdca8b850
rlaclghks123 <rlaclghks123@naver.com>
2024-01-12 15:24:49 +0900

    feat: 주문 아이템을 불러오는 요청의 로딩 표시 구현

--- src/api/orderList.js
+++ src/api/orderList.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const orderList = async () => {
-  const { data } = await axios.get('http://localhost:3001/itemㄴs');
+  const { data } = await axios.get('http://localhost:3001/items');
   return data;
 };
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import WebViewLayout from '../layout/WebViewLayout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
 import { useRecoilState } from 'recoil';
 import { orderListAtom } from '../atoms/orderListAtom';
 import { useQuery } from 'react-query';
@@ -14,10 +15,28 @@ function OrderPage() {
     onSuccess: (data) => setOrderState(data),
   });
 
+  console.log(orderState);
+
   return (
     <WebViewLayout>
       <Wrapper>
         <Header />
+
+        <Main>
+          {error && (
+            <LoadingWrapper>
+              <Section>데이터를 읽어오던 중</Section>
+              <Section>문제가 발생했습니다.</Section>
+            </LoadingWrapper>
+          )}
+
+          {isLoading && (
+            <LoadingWrapper>
+              <Section>목록을</Section>
+              <Section>불러오고 있습니다.</Section>
+            </LoadingWrapper>
+          )}
+        </Main>
       </Wrapper>
       <Footer />
     </WebViewLayout>
@@ -27,10 +46,35 @@ function OrderPage() {
 export default OrderPage;
 
 const Wrapper = styled.div`
+  width: inherit;
+  height: inherit;
+`;
+
+const Main = styled.main`
+  width: inherit;
+  height: inherit;
+`;
+
+const LoadingWrapper = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 
   width: inherit;
   height: inherit;
+`;
+
+const Section = styled.section`
+  width: 221px;
+  height: 45px;
+  flex-shrink: 0;
+  color: #000;
+
+  text-align: center;
+  font-family: Inter;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
