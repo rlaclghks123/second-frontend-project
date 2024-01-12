152a10b9f76697c958cb762923067a2c741bb499
rlaclghks123 <rlaclghks123@naver.com>
2024-01-12 14:48:57 +0900

    chore: react-query 설치 및 세팅

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <GlobalStyle />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <App />
+      </QueryClientProvider>
     </RecoilRoot>
-    ;
   </React.StrictMode>
 );
