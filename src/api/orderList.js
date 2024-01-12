d15e87ee57824b2059608142f3334f3b9068938d
rlaclghks123 <rlaclghks123@naver.com>
2024-01-12 14:56:23 +0900

    feat: 주문 데이터를 받아와 전역 상태에 저장하도록 구현

---
+++ src/api/orderList.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const orderList = async () => {
+  const { data } = await axios.get('http://localhost:3001/itemㄴs');
+  return data;
+};
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -2,8 +2,18 @@ import styled from 'styled-components';
 import WebViewLayout from '../layout/WebViewLayout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useRecoilState } from 'recoil';
+import { orderListAtom } from '../atoms/orderListAtom';
+import { useQuery } from 'react-query';
+import { orderList } from '../api/orderList';
 
 function OrderPage() {
+  const [orderState, setOrderState] = useRecoilState(orderListAtom);
+
+  const { isLoading, error } = useQuery('orderList', orderList, {
+    onSuccess: (data) => setOrderState(data),
+  });
+
   return (
     <WebViewLayout>
       <Wrapper>
